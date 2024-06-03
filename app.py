f54afb7ff27c2ff74cec303563528c7d2e4bf26e
Your Name~ <mthomas4773@gmail.com>
2024-06-03 20:17:07 +1200

    Added a search for books released in a year, fixed bugs and improved visuals, Started Working on a function to add data

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def book_names_and_authors():
         print(f"| {data[0]:<50}| {data[1]:<20}|")
     print("---------------------------------------------------------------------------")
     print("| Book Title                                        | Author Name         |")
+    print("---------------------------------------------------------------------------")
     db.close()
 def book_names_and_author_and_wordcount_greater_than_150000():
     '''Function to display all the books, their word counts and their authors when the word count exceeds 150000'''
@@ -41,8 +42,9 @@ def book_names_and_author_and_wordcount_greater_than_150000():
     print("-----------------------------------------------------------------------------------------")
     for data in results:
         print(f"| {data[0]:<50}| {data[1]:<12}| {data[2]:<20}|")
-    print("----------------------------------------------------------------------------------------")
+    print("-----------------------------------------------------------------------------------------")
     print("| Book Title                                        | Word Count  | Author Name         |")
+    print("-----------------------------------------------------------------------------------------")
     db.close()
 def book_names_and_page_count_and_author_and_genre_pages_less_than_333():
     '''Function to display all the books, their page counts, author, and genre, where there are less than 333 pages'''
@@ -60,6 +62,7 @@ def book_names_and_page_count_and_author_and_genre_pages_less_than_333():
         print(f"| {data[0]:<50}| {data[1]:<12}| {data[2]:<20}| {data[3]:<15}|")
     print("----------------------------------------------------------------------------------------------------------")
     print("| Book Title                                        | Page Count  | Author Name         | Genre          |")
+    print("----------------------------------------------------------------------------------------------------------")
     db.close()
 def book_names_and_authors_and_genres():
     '''Function to display all the books, their authors, and their genre'''
@@ -77,6 +80,7 @@ def book_names_and_authors_and_genres():
         print(f"| {data[0]:<50}| {data[1]:<20}| {data[2]:<12}|")
     print("-----------------------------------------------------------------------------------------")
     print("| Book Title                                        | Author Name         | Genre       |")
+    print("-----------------------------------------------------------------------------------------")
     db.close()
 def book_names_and_authors_and_release_date():
     '''Function to display all the books, their authors, and their release date'''
@@ -89,11 +93,12 @@ def book_names_and_authors_and_release_date():
     results = cursor.fetchall()
     #Print out the Results
     print("| Book Title                                        | Author Name         | Release Date   |")
-    print("-------------------------------------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------")
     for data in results:
         print(f"| {data[0]:<50}| {data[1]:<20}| {data[2]:<14}|")
-    print("-------------------------------------------------------------------------------------------")
+    print("--------------------------------------------------------------------------------------------")
     print("| Book Title                                        | Author Name         | Release Date   |")
+    print("--------------------------------------------------------------------------------------------")
     db.close()
 def book_names_and_author_and_my_enjoyment_and_general_reviews():
     '''Function to display all the books, their authors, my enjoyment, and their general review average'''
@@ -112,6 +117,7 @@ def book_names_and_author_and_my_enjoyment_and_general_reviews():
         print(f"| {data[0]:<50}| {data[1]:<20}| {data[2]:<10}| {data[3]:<8}|")
     print("-------------------------------------------------------------------------------------------------")
     print("| Book Title                                        | Author Name         | Enjoyment | Reviews |")
+    print("-------------------------------------------------------------------------------------------------")
     db.close()
 def book_names_and_isbn_13_classification_and_release_date():
     '''Function to display all the books, their isbn-13 classification, and their release date'''
@@ -130,7 +136,52 @@ def book_names_and_isbn_13_classification_and_release_date():
         print(f"| {data[0]:<50}| {data[1]:<14}| {data[2]:<13}|")
     print("------------------------------------------------------------------------------------")
     print("| Book Title                                        | ISBN          | Release Date |")
+    print("------------------------------------------------------------------------------------")
     db.close()
+def search_for_books_released_in_a_year():
+    '''Function to display book titles, authors, and release dates based on a used input of a year'''
+    search_query = None
+    while search_query == None:
+        try:
+            search_query = int(input("Enter which year's book releases you would like to view! (Positive Numbers Only Please!) "))
+            if search_query <= 0:
+                search_query = 0
+                print("Please enter a positive whole number")
+        except ValueError:
+            print("Please enter a series of positive whole numbers")
+    #Establish Interface
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    #Execute and Fetch Data, Sorting by the year provided
+    query = f"SELECT book.name, author.name, book.release_date FROM book JOIN author ON author.id = book.author WHERE release_date LIKE '{search_query}/%'"
+    cursor.execute(query)
+    results = cursor.fetchall()
+    #Print out the Results
+    if len(results) != 0:
+        print("| Book Title                                        | Author              | Release Date |")
+        print("------------------------------------------------------------------------------------------")
+        for data in results:
+            print(f"| {data[0]:<50}| {data[1]:<20}| {data[2]:<13}|")
+        print("------------------------------------------------------------------------------------------")
+        print("| Book Title                                        | Author              | Release Date |")
+        print("------------------------------------------------------------------------------------------")
+    else:
+        print("It appears there was no data for that year!")
+    db.close()
+def add_data_to_a_table():
+    #Establish Interface
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    #Execute and Fetch Data
+    tables = ['book', 'author', 'genre']
+    print("Enter")
+    query = f"PRAGMA table_info({tables[table_id-1]})"
+    cursor.execute(query)
+    column_data = cursor.fetchall()
+    print(column_data)
+
+
+
 
 #Establish Array of Functions
 function_array = {
@@ -141,6 +192,8 @@ function_array = {
     "5": book_names_and_authors_and_release_date,
     "6": book_names_and_author_and_my_enjoyment_and_general_reviews,
     "7": book_names_and_isbn_13_classification_and_release_date,
+    "8": search_for_books_released_in_a_year,
+    "9": add_data_to_a_table,
     "quit": quit,
 }
 
@@ -148,8 +201,8 @@ function_array = {
 
 #Major Code Loop
 if __name__ == "__main__":
-    #Loop to Provide ADD TO THIS NOW 
     while True:
+        #Code to give the user a series of function options, these include prewritten querries, searches (WIP), and adding/removing data
         print('Use the string quit without providing an input to close the program')
         print('\n')
         print('1. Print all the book titles and authors')
@@ -159,10 +212,11 @@ if __name__ == "__main__":
         print('5. Print all the book titles, authors, and release dates')
         print('6. Print all the book titles, authors, my enjoyment, and the general reviews')
         print('7. Print all the book titles, isbn-13 numbers, and release dates')
+        print('8. Search for all the books released in a year')
         user_input = input('Input the number associated with the function you wish to execute. \n')
         print("\n")
         if user_input in function_array.keys():
-            function_array[user_input]()
+            function_array[user_input.lower()]()
             time.sleep(2)
         else:
             print("Invalid input, please enter the number before a function without any trailing spaces or symbols!")
